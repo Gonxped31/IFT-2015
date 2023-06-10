4e88c75c85075a07188a0fbd8da2491372436384
gonxped31 <gonxped31@gmail.com>
2023-06-10 15:15:09 -0400

    added zip file

--- TP1/Tp1/Tp1.java
+++ TP1/Tp1/Tp1.java
@@ -18,7 +18,6 @@ public class Tp1 {
         LinkedList<LinkedList<Double>> ordoredLists = ordoredLists(dist, boxs, buldi);
         LinkedList<LinkedList<Double>> updateOrdoredLists = updateOrdoredLists(ordoredLists, boxsData);
         writer(args[1], updateOrdoredLists, truckpos);
-        empiricTest(50);
     }
 
     // Return a LinkedList with the number of box to transport and the truck max capacity.
@@ -341,47 +340,6 @@ public class Tp1 {
 
         } catch (IOException exception) {
             exception.printStackTrace();
-        }
-    }
-
-    public static void empiricTest(int inputsNumber) {
-        LinkedList<Long> datas = new LinkedList<>();
-        try {
-            LinkedList<String> inputs = new LinkedList<>();
-            for (int i = 100; i <= inputsNumber*50; i += 50) {
-                String a = "" + i;
-                inputs.add(a);
-            }
-
-            for (int i = 0; i < inputsNumber-1; ++i) {
-                String inputsPaths = ".\\exemplaires_dev1\\";
-                inputsPaths += inputs.get(i) + ".txt";
-                long startTime = System.currentTimeMillis();
-                LinkedList<Integer> boxsData = boxsData(inputsPaths);
-                LinkedList<Double> buldi = buildings(inputsPaths);
-                LinkedList<Double> boxs = numberOfBoxsPerbuilding(inputsPaths);
-                LinkedList<Double> truckpos = truckPosition(boxs, buldi);
-                LinkedList<Double> dist = distances(buldi, truckpos);
-
-                LinkedList<LinkedList<Double>> ordoredLists = ordoredLists(dist, boxs, buldi);
-                updateOrdoredLists(ordoredLists, boxsData);
-                long endTime = System.currentTimeMillis();
-
-                datas.add(endTime - startTime);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-
-        try (BufferedWriter bWriter = new BufferedWriter(new FileWriter("EmpiricTestData.txt"))) {
-            for (long data : datas) {
-                bWriter.append("" + data);
-                bWriter.newLine();
-            }
-            bWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
